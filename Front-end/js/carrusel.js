4626cf3196c1d57919626fd178df0b85c56f6959
rachel11072003 <rachel11072003@gmail.com>
2023-08-01 15:15:51 -0500

    Se agregan cambios adicionales

--- Front-end/js/carrusel.js
+++ Front-end/js/carrusel.js
@@ -1,12 +1,74 @@
 //CARRUSEL PANTALLAS PEQUEÑAS
-$(".custom-carousel").owlCarousel({
+/*$(".custom-carousel").owlCarousel({
     autoWidth: true,
     loop: true
-  });
+  });*/
   $(document).ready(function () {
     $(".custom-carousel .item").click(function () {
       $(".custom-carousel .item").not($(this)).removeClass("active");
       $(this).toggleClass("active");
     });
   });
+/*
+  $(document).ready(function () {
+    // Inicializar el carrusel con Owl Carousel
+    $(".custom-carousel").owlCarousel({
+      autoWidth: true,
+      loop: true,
+      animateOut: 'fadeOut', // Agregar animación de desvanecimiento suave
+      animateIn: 'fadeIn', // Agregar animación de desvanecimiento suave al volver al primer elemento
+    });
+  function moveCarousel() {
+    $(".custom-carousel").trigger("next.owl.carousel");
+  }
+
+  // Ejecutar la función moveCarousel cada 3 segundos
+  setInterval(moveCarousel, 2000);
+});
+
+ // Detener el intervalo cuando se hace clic en un elemento del carrusel
+ $(".custom-carousel .item").click(function () {
+  $(".custom-carousel .item").not($(this)).removeClass("active");
+  $(this).toggleClass("active");
+
+  // Detener el intervalo usando la función clearInterval
+  clearInterval(moveCarousel);
+});*/
+
+$(document).ready(function () {
+  let carouselInterval; // Variable para almacenar el ID del intervalo
+
+  // Función para mover el carrusel automáticamente
+  function moveCarousel() {
+    $(".custom-carousel").trigger("next.owl.carousel");
+  }
+})
+  // Inicializar el carrusel con Owl Carousel
+  $(document).ready(function () {
+    let isCarouselPlaying = true;
+
+    // Inicializar el carrusel con Owl Carousel
+    const carousel = $(".custom-carousel").owlCarousel({
+      autoWidth: true,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      animateOut: 'fadeOut',
+      animateIn: 'fadeIn',
+    });
+
   
+    // Detener o reanudar el carrusel cuando se hace clic en un elemento
+    $(".custom-carousel .item").click(function () {
+      if (isCarouselPlaying) {
+        carousel.trigger('stop.owl.autoplay');
+      } else {
+        carousel.trigger('play.owl.autoplay');
+      }
+      isCarouselPlaying = !isCarouselPlaying;
+      
+    });
+      
+
+  });
+
---
+++ Front-end/js/phone.js
@@ -0,0 +1,25 @@
+import { PhoneNumberUtil, PhoneNumberFormat } from 'libphonenumber-js';
+
+const phoneNumberUtil = PhoneNumberUtil.getInstance();
+
+// Manejar el evento click del botón "Enviar"
+const submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', () => {
+  const countrySelect = document.getElementById('country');
+  const countryCode = countrySelect.value;
+
+  const phoneNumberInput = document.getElementById('phoneNumberInput');
+  const phoneNumberStr = phoneNumberInput.value;
+
+  try {
+    const phoneNumber = phoneNumberUtil.parse(phoneNumberStr, countryCode);
+    const isValid = phoneNumberUtil.isValidNumber(phoneNumber);
+    const formattedNumber = phoneNumberUtil.format(phoneNumber, PhoneNumberFormat.INTERNATIONAL);
+
+    console.log('Número de teléfono parseado:', phoneNumber);
+    console.log('El número de teléfono es válido:', isValid);
+    console.log('Número de teléfono formateado:', formattedNumber);
+  } catch (error) {
+    console.error('Error al parsear el número de teléfono:', error);
+  }
+});
